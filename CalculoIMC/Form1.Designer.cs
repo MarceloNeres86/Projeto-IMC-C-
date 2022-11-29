73accf1374fde5abd3537ec1bca34d3a5f99750b
MarceloNeres86 <marcelo.neres@hotmail.com>
2022-11-28 21:39:02 -0300

    calculo imc V1 C#

---
+++ CalculoIMC/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace CalculoIMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbSexo = new System.Windows.Forms.ComboBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.btnPesoideal = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Titulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbSexo
+            // 
+            this.cmbSexo.BackColor = System.Drawing.Color.AliceBlue;
+            this.cmbSexo.FormattingEnabled = true;
+            this.cmbSexo.Items.AddRange(new object[] {
+            "Feminino",
+            "Masculino"});
+            this.cmbSexo.Location = new System.Drawing.Point(386, 298);
+            this.cmbSexo.Name = "cmbSexo";
+            this.cmbSexo.Size = new System.Drawing.Size(257, 30);
+            this.cmbSexo.TabIndex = 2;
+            this.cmbSexo.SelectedIndexChanged += new System.EventHandler(this.cmbSexo_SelectedIndexChanged);
+            // 
+            // txtNome
+            // 
+            this.txtNome.BackColor = System.Drawing.Color.AliceBlue;
+            this.txtNome.Location = new System.Drawing.Point(91, 228);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(552, 30);
+            this.txtNome.TabIndex = 3;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.BackColor = System.Drawing.Color.AliceBlue;
+            this.txtAltura.Location = new System.Drawing.Point(91, 298);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(241, 30);
+            this.txtAltura.TabIndex = 5;
+            // 
+            // btnPesoideal
+            // 
+            this.btnPesoideal.BackColor = System.Drawing.Color.Indigo;
+            this.btnPesoideal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPesoideal.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPesoideal.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnPesoideal.Location = new System.Drawing.Point(91, 481);
+            this.btnPesoideal.Name = "btnPesoideal";
+            this.btnPesoideal.Size = new System.Drawing.Size(170, 40);
+            this.btnPesoideal.TabIndex = 6;
+            this.btnPesoideal.Text = "Peso &Ideal";
+            this.btnPesoideal.UseVisualStyleBackColor = false;
+            this.btnPesoideal.Click += new System.EventHandler(this.btnPesoideal_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.Indigo;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnLimpar.Location = new System.Drawing.Point(287, 481);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(170, 40);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Indigo;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnSair.Location = new System.Drawing.Point(488, 481);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(170, 40);
+            this.btnSair.TabIndex = 8;
+            this.btnSair.Text = "&Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(92, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 21);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nome";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(92, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 21);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Altura";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Location = new System.Drawing.Point(388, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 21);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Sexo";
+            // 
+            // Titulo
+            // 
+            this.Titulo.AutoSize = true;
+            this.Titulo.BackColor = System.Drawing.Color.Transparent;
+            this.Titulo.Font = new System.Drawing.Font("Cooper Black", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titulo.ForeColor = System.Drawing.Color.MintCream;
+            this.Titulo.Location = new System.Drawing.Point(195, 82);
+            this.Titulo.Name = "Titulo";
+            this.Titulo.Size = new System.Drawing.Size(357, 42);
+            this.Titulo.TabIndex = 12;
+            this.Titulo.Text = "Calcule o Seu Peso";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CalculoIMC.Properties.Resources.peso_ideal;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(770, 565);
+            this.Controls.Add(this.Titulo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnPesoideal);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.cmbSexo);
+            this.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbSexo;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Button btnPesoideal;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Titulo;
+    }
+}
+
---
+++ CalculoIMC/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoIMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNome.Clear();
+            txtAltura.Clear();
+            cmbSexo.Text = "";
+            txtNome.Focus();
+
+        }
+
+        private void cmbSexo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPesoideal_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Double Altura = 0, Pesoideal = 0;
+                Altura = double.Parse(txtAltura.Text);
+                if (cmbSexo.Text == "Feminino")
+                {
+                    Pesoideal = (62.7 * Altura) - 44.7;
+                    MessageBox.Show(txtNome.Text + " Seu Peso ideal é: " + Pesoideal.ToString() + " Kg",
+                        "Peso Ideal", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else if (cmbSexo.Text == "Masculino")
+                {
+                    Pesoideal = (72.7 * Altura) - 58;
+                    MessageBox.Show(txtNome.Text + " Seu Peso ideal é: " + Pesoideal.ToString() + " Kg",
+                        "Peso Ideal", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Escolha o sexo", "Peso Ideal", MessageBoxButtons.OK,
+                        MessageBoxIcon.Exclamation);
+                }
+            }
+            catch (Exception)
+            {
+
+               MessageBox.Show("Verifique os valores informados" , "Peso Ideal",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
